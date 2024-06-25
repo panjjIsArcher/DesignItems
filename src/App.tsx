73167e9ed4cfd78e512653fbd041dfdbb9e71955
panjiajie <panjiajie@chexiao.co>
2024-06-25 18:27:44 +0800

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 
 import AppStyle from "./assets/App.module.scss"
+import IndexPage from "./view/index/index"
 function App() {
   return (
     <div className={AppStyle.index}>
-
+			<IndexPage/>
 		</div>
   )
 }
