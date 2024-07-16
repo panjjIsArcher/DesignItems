504de9e61d07be5ff9a84197f15d41b16a132be5
panjiajie <panjiajie@chexiao.co>
2024-07-16 17:37:14 +0800

    spread

--- src/mock/database.ts
+++ src/mock/database.ts
@@ -3,6 +3,7 @@ import { BASE_CARD } from "../type/card";
 const cards: BASE_CARD[] = [
   { type: "petanca", className: "petanca-btn", text: "petanca" },
   { type: "flash", className: "flash-btn", text: "flash" },
+  { type: "spread", className: "spread-btn", text: "spread" },
 ];
 
 export { cards };
